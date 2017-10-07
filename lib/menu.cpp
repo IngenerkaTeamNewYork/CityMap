45d56664f64fcdd645a8c4b0319ab4261ce1163e
quwinner <dancatop@yandex.ru>
2017-10-07 15:46:01 +0300

    Заполнение меню

--- lib/menu.cpp
+++ lib/menu.cpp
@@ -5,7 +5,7 @@ struct BUTTON2
 {
     const char* textbutton;
     int minX;
-    int maxX;
+    int minY;
     bool isPushed;
 };
 
@@ -15,9 +15,24 @@ struct BUTTON
     int minX;
     int maxX;
     bool isPushed;
-    BUTTON2 knopki[10];
+    BUTTON2 knopki[15];
 };
 
+void zapolnenie_mosiva1(BUTTON* button)
+{
+    button->knopki[0] =  {"KrasivyiDom",    button->minX, 100, false};
+    button->knopki[1] =  {"Cottage",        button->minX, 125, false};
+    button->knopki[2] =  {"Domische",       button->minX, 150, false};
+    button->knopki[3] =  {"SkyScraper",     button->minX, 175, false};
+    button->knopki[4] =  {"House",          button->minX, 200, false};
+    button->knopki[5] =  {"Dacha",          button->minX, 225, false};
+    button->knopki[6] =  {"TipichnyiDom",   button->minX, 250, false};
+    button->knopki[7] =  {"Hotel",          button->minX, 275, false};
+    button->knopki[8] =  {"MGU",            button->minX, 300, false};
+    button->knopki[9] =  {"StatuyaSvobody", button->minX, 325, false};
+    button->knopki[10] = {"Eiffel",         button->minX, 350, false};
+
+}
 void menu_draw(BUTTON* button)
 {
     txSetColor (TX_LIGHTBLUE);
@@ -27,6 +42,8 @@ void menu_draw(BUTTON* button)
 
 void menu_focus(BUTTON* button)
 {
+    txSetColor (TX_LIGHTBLUE);
+    txSetFillColor (RGB(180,250,250));
     //Нажали на кнопку
     if ((txMouseY() > 50  && txMouseY() < 100)
      && (txMouseX () > button->minX && txMouseX () < button->maxX))
@@ -42,7 +59,10 @@ void menu_focus(BUTTON* button)
     if (button->isPushed)
     {
         txRectangle (button->minX, 100, button->maxX, 500);
-
+        for(int i = 0; i < 11; i++)
+        {
+        txTextOut(button->knopki[i].minX, button->knopki[i].minY, button->knopki[i].textbutton);
+        }
         //Фокус на нужной подкнопке
         if (txMouseY() > 250  && txMouseY() < 300)
         {
