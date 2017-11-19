cf5be3af57dfb672d00e5c859f6976f163c9daa2
mabramovsd <beavisabra@yandex.ru>
2017-11-19 08:00:34 +0300

    file.cpp in Lib, delete rubbish

--- lib/file.cpp
+++ lib/file.cpp
@@ -12,13 +12,13 @@ using namespace std;
 int massive(CartincaNaKarte* KART1)
 {
     ifstream fout;
-    fout.open("file.txt");
+    fout.open("Files\\Openfile.txt");
     string imya_faila;
     getline (fout, imya_faila);
     fout.close();
 
     fout.open(imya_faila);
-    //txCreateWindow(800, 600);
+
     int nomer = 0;
     while (fout.good()) {
         string get;
@@ -34,18 +34,27 @@ int massive(CartincaNaKarte* KART1)
 
         int pos3 = get.find(',');
         string y = get.substr(0, pos3);
-        get = get.substr(pos3 + 2);
-
-        //string bools = get;
-
-        /*HDC pic = txLoadImage(adress.c_str());
-        txBitBlt (txDC(), atoi(x.c_str()), atoi(y.c_str()), 30, 30, pic, 0, 0);
-        txDeleteDC(pic);*/
 
         KART1[nomer] = {txLoadImage(adress.c_str()), atoi(x.c_str()), atoi(y.c_str()), true};
         nomer++;
     }
+
     fout.close();
 
     return nomer;
+}
+
+void saveMassive()
+{
+    ofstream fout;
+    fout.open("gfdg.txt");
+
+    CartincaNaKarte KART[1];
+    KART[0] = {txLoadImage("Icons\\Monuments\\Lenin.bmp"), 200, 200, true, "Icons\\Monuments\\Lenin.bmp"};
+    for(int i = 0; i< 1; i++)
+    {
+        fout << KART[i].adress << "," << KART[i].X << "," << KART[i].Y << endl;
+    }
+
+    fout.close();
 }
